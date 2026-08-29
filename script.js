329b23a4ad3ca5d9f9a451963544f09d2baace66
bruhhnah73-collab <bruhhnah73@gmail.com>
2026-08-29 09:40:11 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const themeToggle = document.getElementById("themeToggle");
 
+themeToggle.addEventListener("click", function () {
+
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeToggle.textContent = "🌙 Dark Mode";
+    } else {
+        themeToggle.textContent = "☀️ Light Mode";
+    }
+
+});
